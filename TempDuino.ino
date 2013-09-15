fe0efb342748d3917fe7be71924a52a8cf6d7895
Raphael Silva Souza <raphaelssouza@outlook.com>
2013-09-15 14:05:05 -0300

    Update TempDuino.ino
    Adequando código ao que está rodando

--- TempDuino.ino
+++ TempDuino.ino
@@ -1,6 +1,4 @@
-/*
-Adafruit Arduino - Lesson 12. Light and Temperature
-*/
+
 #include <LiquidCrystal.h>
 
 int tempPin = 0;
@@ -17,11 +15,11 @@ void setup()
 
 void loop()
 {
-// Display Temperature in C
+  //Mostrar Temperatura em º Celsius
   int tempReading = analogRead(tempPin);
   float tempVolts = tempReading * 0.00488;
   float tempC = tempVolts * 100;
-   // ----------------
+  
   lcd.setCursor(0, 0);
   lcd.print("Temp.");
   lcd.setCursor(8, 0);
@@ -29,12 +27,13 @@ void loop()
   lcd.createChar(1, a); //Atribui a "1" o valor do array "A", que desenha o simbolo de grau
   lcd.setCursor(13,0); //Coloca o cursor na coluna 7, linha 1
   lcd.write(1); //Escreve o simbolo de grau
-  // Display Light on second row
+  
+  // Mostrar informação sobre luminosidade na segunda linha
   int lightReading = analogRead(lightPin);
   lcd.setCursor(0, 1);
   lcd.print("Light");
   lcd.setCursor(8, 1);
   lcd.print(lightReading);
-  delay(5000);
+  delay(5000); // Tempo de espera para atualização de 5 segundos
 }
 
